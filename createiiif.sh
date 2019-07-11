69fc05958a04f040c68154ddb46f3afb8f4adcb1
Soumik <soumik@soumikghosh.com>
2019-07-11 15:56:05 -0400

    And environment variables and assorted changes

--- createiiif.sh
+++ createiiif.sh
@@ -2,8 +2,10 @@
 # Create directory structure (IIIF script requires it)
 mkdir -p ${ACCESS_DIR}
 # Fetch the images
-aws s3 sync s3://${SRC_BUCKET}/${ACCESS_DIR} ${ACCESS_DIR}
+aws s3 sync s3://${SRC_BUCKET}/${DIR_PREFIX}/${ACCESS_DIR} ${ACCESS_DIR}
 # Fetch the CSV file
 aws s3 sync s3://${SRC_BUCKET}/${CSV_PATH} .
 # Generate the tiles
-ruby create_iiif_s3.rb ${CSV_NAME} ${ACCESS_DIR} ${DEST_URL} ${DEST_BUCKET} --upload_to_s3=${UPLOAD_BOOL}
+ruby create_iiif_s3.rb ${CSV_NAME} ${ACCESS_DIR}/ ${DEST_URL} ${DEST_BUCKET} --upload_to_s3=${UPLOAD_BOOL}
+# Upload generated tiles
+aws s3 sync tmp/${DEST_BUCKET}/ s3://${DEST_BUCKET}
