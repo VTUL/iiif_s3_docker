361e55e6d41d19719bea3ff72e21a9a817a4ae2e
Lee Hunter <whunter@vt.edu>
2024-07-07 08:31:50 -0400

    start full iterative refactor

--- create_iiif_s3.rb
+++ create_iiif_s3.rb
@@ -3,6 +3,8 @@
 # A generator for IIIF compatible image tiles and metadata
 # Try "./create_iiif_s3.rb -h"
 #
+require 'aws-sdk'
+require 'fileutils'
 require 'iiif_s3'
 require 'open-uri'
 require 'optparse'
@@ -40,6 +42,14 @@ def get_metadata(csv_url, id)
   end
 end
 
+def get_s3_filelist_for_item(s3, options)
+  resp = s3.list_objects_v2({
+    bucket: options[:source_bucket],
+    prefix: options[:image_folder]
+  })
+  return resp.contents.map{ |f| f.key }
+end
+
 def add_image(file, id, idx)
   name = File.basename(file, File.extname(file))
   page_num = idx + 1
@@ -57,6 +67,7 @@ def add_image(file, id, idx)
   obj["section"] = "p#{page_num}"
   obj["section_label"] = "Page #{page_num}"
   @data.push IiifS3::ImageRecord.new(obj)
+  puts obj.inspect
 end
 
 def is_image_file?(file)
@@ -66,11 +77,24 @@ end
 
 options = {}
 optparse = OptionParser.new do |parser|
-  parser.banner = "Usage: create_iiif_s3.rb -c collection_identifer -m csv_metadata_file -i image_folder_path -b metadata_base_path -r dest_root_folder"
+  parser.banner = "Usage: create_iiif_s3.rb -s source_bucket -d dest_bucket -c collection_identifer -m csv_metadata_file -i image_folder_path -b metadata_base_path -r dest_root_folder"
 
+  # /usr/local/iiif/tmp/tmp.Nvs5NF9cpD
   # short option, long option, description of the option
+  parser.on("-t", "--tmp_dir Name", "Temp dir") do |source_bucket|
+    options[:tmp_dir] = source_bucket
+  end
+  parser.on("-s", "--source_bucket Name", "AWS Source Bucket") do |source_bucket|
+    options[:source_bucket] = source_bucket
+  end
+  parser.on("-d", "--dest_bucket Name", "AWS Destination Bucket") do |dest_bucket|
+    options[:dest_bucket] = dest_bucket
+  end
   parser.on("-c", "--collection_identifier ID", "Parent collection identifier") do |collection_identifier|
     options[:collection_identifier] = collection_identifier
+  end
+  parser.on("-p", "--metadata_path Path", "CSV path") do |metadata_path|
+    options[:metadata_path] = metadata_path
   end
   parser.on("-m", "--metadata_file File", "Metadata CSV file") do |metadata_file|
     options[:metadata_file] = metadata_file
@@ -84,41 +108,50 @@ optparse = OptionParser.new do |parser|
   parser.on("-r", "--dest_root_folder Path", "Path to root folder") do |dest_root_folder|
     options[:dest_root_folder] = dest_root_folder
   end
-  # s3 upload is handled in the bash calling script after tiling completes
-  options[:upload_to_s3] = false
   parser.on_tail("-h", "--help", "Prints this help") do
     puts parser
     exit
   end
 end.parse!
 
-unless @csv_url = options[:metadata_file]
-  puts "Please provide metadata_file name."
+FileUtils.mkdir_p(options[:tmp_dir]) unless Dir.exists?(options[:tmp_dir])
+# s3 upload is handled in the bash calling script after tiling completes
+options[:upload_to_s3] = false
+
+credentials = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+s3_client = Aws::S3::Client.new(
+  region: "us-east-1",
+  credentials: credentials
+)
+s3_resource = Aws::S3::Resource.new(client: s3_client)
+
+# begin
+  csv_key = options[:metadata_path] + "/" + options[:metadata_file]
+  @csv_url = options[:tmp_dir] + "/" + options[:metadata_file]
+  File.open(@csv_url, 'wb') do |csv_file|
+    reap = s3_client.get_object({ bucket: options[:source_bucket], key: csv_key }, target: csv_file)
+  end
+  puts File.exists?(@csv_url)
+  puts "Writes csv to s3 here"
+  # s3_client.put_object({ bucket: options[:dest_bucket], key: csv_key, body: File.open(@csv_url) }
+# rescue StandardError => e
+#   puts "An error occurred processing metadata file #{@csv_url}: #{e.message}"
+# end
+# collection pattern, e.g., Ms1990_025, is legacy and no longer required
+collection_identifier = options[:collection_identifier]
+unless image_folder_path = options[:image_folder]
+  puts "Please provide path to image folder."
   puts "Try './create_iiif_s3.rb -h'"
   exit
 else
   begin
-    # collection pattern, e.g., Ms1990_025, is legacy and no longer required
-    collection_identifier = options[:collection_identifier]
-    unless image_folder_path = options[:image_folder]
-      puts "Please provide path to image folder."
-      puts "Try './create_iiif_s3.rb -h'"
-      exit
-    else
-      begin
-        @input_folder = image_folder_path.slice(image_folder_path.index("#{collection_identifier}")..-1)
-        puts "Access folder: #{@input_folder}"
-        # # sort image files in the image folder
-        # all_access_image_files = Dir[image_folder_path + "*"]
-        # @image_files = all_access_image_files.select{ |f| is_image_file?(f) }.sort        
-      rescue StandardError => e
-        puts "An error occurred processing image folder at #{image_folder_path}: #{e.message}"
-      end
-    end
+    @input_folder = image_folder_path.slice(image_folder_path.index("#{collection_identifier}")..-1)
+    puts "Access folder: #{@input_folder}"      
   rescue StandardError => e
-      puts "An error occurred process manifest file #{@csv_url}: #{e.message}"
+    puts "An error occurred processing image folder at #{image_folder_path}: #{e.message}"
   end
 end
+
 
 # Setup Temporary stores
 @data = []
@@ -145,6 +178,7 @@ else
   opts[:prefix] = "#{options[:dest_root_folder]}/#{@input_folder.split('/')[0..-3].join('/')}"
 end
 
+
 puts "Instantiating IIIF S3 Builder"
 iiif = IiifS3::Builder.new(opts)
 @config = iiif.config
@@ -153,13 +187,15 @@ puts "IiifS3::Builder configuration: (iiif.config)"
 puts @config.inspect
 # sort image files in the image folder
 begin
-  all_access_image_files = Dir[image_folder_path + "*"]
+  all_access_image_files = get_s3_filelist_for_item(s3_client, options)
   @image_files = all_access_image_files.select{ |f| is_image_file?(f) }.sort
 rescue StandardError => e
   puts "An error occurred processing image folder at #{image_folder_path}: #{e.message}"
 end
 
+
 path = "#{@config.output_dir}#{@config.prefix}/"
+puts "path: #{path}"
 create_directories(path)
 
 # generate a path on disk for "output_dir/prefix/image_dir"
@@ -167,13 +203,20 @@ img_dir = "#{path}#{@config.image_directory_name}/".split("/")[0...-1].join("/")
 create_directories(img_dir)
 
 id = @input_folder.split("/")[-2]
-
 @image_files.each_with_index do |image_file, idx|
+  tmp_path = options[:tmp_dir] + "/" + File.basename(image_file)
+  File.open(tmp_path, 'wb') do |file|
+    reap = s3_client.get_object({ bucket: options[:source_bucket], key: image_file }, target: file)
+  end
+
   puts "Adding image file #{image_file} to iiif data object..."
-  add_image(image_file, id, idx)
+  add_image(tmp_path, id, idx)
+  puts "Passing iiif data object to iiif_s3 gem for processing..."
+  iiif.load(@data)
+  iiif.process_data
 end
-puts "Passing iiif data object to iiif_s3 gem for processing..."
-iiif.load(@data)
-iiif.process_data
+
+puts Dir["#{options[:tmp_dir]}/**/*"]
+
 puts "Processing complete"
 puts "Exiting job."
--- createiiif.sh
+++ createiiif.sh
@@ -12,31 +12,30 @@ cp ../../*.* .
 # Create directory structure (IIIF script requires it)
 echo "Creating directory structure for IIIF script: ${ACCESS_DIR}"
 mkdir -p ${ACCESS_DIR}
-# Fetch the images
-echo "Fetching images from: s3://${AWS_SRC_BUCKET}/${ACCESS_DIR}"
-# Can be helpful to remove "--quiet" flag when testing
-aws s3 sync s3://${AWS_SRC_BUCKET}/${ACCESS_DIR} ${ACCESS_DIR}
-# Fetch the CSV file
-echo "Fetching CSV file from: s3://${AWS_SRC_BUCKET}/${CSV_PATH}/${CSV_NAME}"
-aws s3 cp s3://${AWS_SRC_BUCKET}/${CSV_PATH}/${CSV_NAME} .
+
 # Generate the tiles
 echo "Calling ruby script to generate tiles with the following arguments:"
+echo "IIIF_TEMP: -t ${TMPDIR}"
+echo "SRC_BUCKET: -s ${AWS_SRC_BUCKET}"
+echo "DEST_BUCKET: -d ${AWS_DEST_BUCKET}"
 echo "COLLECTION_IDENTIFIER: -c ${COLLECTION_IDENTIFIER}"
+echo "CSV_PATH: -m ${CSV_PATH}"
 echo "CSV_NAME: -m ${CSV_NAME}"
 echo "ACCESS_DIR: -i ${ACCESS_DIR}"
 echo "DEST_URL: -b ${DEST_URL}"
 echo "DEST_PREFIX -r: ${DEST_PREFIX}"
 
 AWS_BUCKET_NAME=${AWS_DEST_BUCKET} \
-ruby create_iiif_s3.rb -c ${COLLECTION_IDENTIFIER} -m ${CSV_NAME} -i ${ACCESS_DIR}/ -b ${DEST_URL} -r ${DEST_PREFIX}
-# Put CSV files in the proper place
-echo "Copying CSV file to correct directory"
-mkdir -p tmp/${DEST_PREFIX}/${COLLECTION_IDENTIFIER}
-cp ${CSV_NAME} tmp/${DEST_PREFIX}/${COLLECTION_IDENTIFIER}/
-# Upload generated tiles
-echo "Uploading tiles and metadata to dest bucket/path: s3://${AWS_DEST_BUCKET}/${DEST_PREFIX}/"
-# Can be helpful to remove "--quiet" flag when testing
-aws s3 sync tmp/${DEST_PREFIX}/ s3://${AWS_DEST_BUCKET}/${DEST_PREFIX}/ --quiet
+ruby create_iiif_s3.rb \
+    -s ${AWS_SRC_BUCKET} \
+    -d ${AWS_DEST_BUCKET} \
+    -c ${COLLECTION_IDENTIFIER} \
+    -p ${CSV_PATH} \
+    -m ${CSV_NAME} \
+    -i ${ACCESS_DIR}/ \
+    -b ${DEST_URL} \
+    -r ${DEST_PREFIX}
+
 # Delete tmpdir
 echo "Cleaning up: Deleting tmpdir ${TMPDIR}"
 rm -rf ${TMPDIR}
